23b9bb45eb0a191799da6a5f8ded84779cdd74c5
saad <saad.riaz93@gmail.com>
2019-09-22 12:50:49 +0500

    first commit. Setting up the project and basic modules

---
+++ src/app/tables/tables.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TablesComponent } from './tables.component';
+
+
+
+@NgModule({
+  declarations: [TablesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TablesModule { }
